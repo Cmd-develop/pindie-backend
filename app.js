204161755461c6cabff2c4850ebd88ea4d94c8b6
Cmd-develop <150024807+Cmd-develop@users.noreply.github.com>
2024-05-31 08:20:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const apiRouter = require('./routes/apiRouter');
 const pagesRouter = require('./routes/pages');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 connectToDatabase();
 
